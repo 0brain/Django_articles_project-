99930893ee9e4604b5a23fc797214e1dae544464
nazar <nazar.khort@gmail.com>
2021-09-04 22:41:23 +0300

    Set up admin dashboard for Category table, created list_editable and list_filter for News table

--- news/models.py
+++ news/models.py
@@ -10,7 +10,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Час оновлення')
     photo = models.ImageField(upload_to='photos/%y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубліковано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категорія')
 
     def __str__(self):
         return self.title
@@ -23,6 +23,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Назва категорії')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Категорія'

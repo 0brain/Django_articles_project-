becca0a2bea37e955a06ad6a07ea90fbaa1447fe
nazar <nazar.khort@gmail.com>
2021-09-05 15:20:57 +0300

    Displayed categories list at index.html

--- news/views.py
+++ news/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    return render(request, template_name='news/index.html', context={'news': news, 'title': "Список новин"})
+    categories = Category.objects.all()
+    context = {'news': news,
+             'title': "Список новин",
+             'categories': categories,
+    }
+    return render(request, template_name='news/index.html', context=context)
 

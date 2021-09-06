0361e6a8220f0f17b4edee96011d2748eb18182b
nazar <nazar.khort@gmail.com>
2021-09-06 10:23:49 +0300

    Created view_news function and made path for view_news.html.

--- news/views.py
+++ news/views.py
@@ -19,3 +19,8 @@ def get_category(request, category_id):
                'category': category,
     }
     return render(request, template_name='news/category.html', context=context)
+
+
+def view_news(request, news_id):
+    news_item = News.objects.get(pk=news_id)
+    return render(request, template_name='news/view_news.html', context={'news_item': news_item})

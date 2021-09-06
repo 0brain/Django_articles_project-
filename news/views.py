d4675d171f76004a30e7356cd2a98a6b7bec31f1
nazar <nazar.khort@gmail.com>
2021-09-06 19:57:51 +0300

    Set up class NewsForm, create post process in def add_news, updated add_news.html.

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
 from .forms import NewsForm
@@ -28,8 +28,11 @@ def view_news(request, news_id):
 
 
 def add_news(request):
-    if request.method == 'Post':
-        pass
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})

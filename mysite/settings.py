d44223bb0f4c94aa2a681f567ffef74c417664ea
nazar <nazar.khort@gmail.com>
2021-09-06 12:20:01 +0300

    Uploaded css and js files. Created collectstatic and connected css and js to base.html.

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,10 +115,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
